9960bdcd5788b3cbeb73bd52cfbd29e60900dabe
Richard Day <Richard.Day@polygonuk.com>
2020-10-05 17:34:33 +0100

    the story so far

--- ElementList.cs
+++ ElementList.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Dynamic;
+using System.Linq;
 using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Support.UI;
 
 namespace LloydsDCAutomation
 {
     public class ElementList
     {
-        private readonly ChromeDriver _driver;
+        private readonly EdgeDriver _driver;
         private readonly WebDriverWait _wait;
 
-        public ElementList(ChromeDriver driver, WebDriverWait wait)
+        public ElementList(EdgeDriver driver, WebDriverWait wait)
         {
             _driver = driver;
             _wait = wait;
@@ -23,12 +26,15 @@ namespace LloydsDCAutomation
         public void Launch()
         {
 
+
             _driver.Navigate()
                 .GoToUrl("https://homeclaims.lloydsbank.com/sap/bc/ui5_ui5/ui2/ushell/shells/abap/Fiorilaunchpad.html");
 
             _driver.Manage().Cookies.DeleteAllCookies();
 
             _driver.Navigate().Refresh();
+
+            _driver.Manage().Window.Maximize();
 
 
         }
@@ -93,6 +99,41 @@ namespace LloydsDCAutomation
             }
         }
 
+        public IWebElement UploadFile
+        {
+            get
+            {
+                return _wait.Until(x =>
+                    x.FindElement(By.XPath(
+                        "/html/body/div[1]/div[2]/section/div/div/div/div/div/div/div[3]/div/div/div/div[2]/div/div/div/form/div/div[2]/input[1]")));
+            }
+        }
+
+        public IWebElement Participants
+        {
+            get
+            {
+                return _wait.Until(x =>
+                    x.FindElement(By.XPath(
+                        "/html/body/div[4]/div/div/div/div/div[2]/section/div/div[2]/div[3]/section/div/div/div/div[2]/div/div[2]/div[3]/section/div/div/div/div/div/div[3]/div/div/div/section/div[2]/div[1]/div/div/div[5]/div[1]/span[1]")));
+            }
+        }
+
+        public IWebElement ParticipantsList
+        {
+            get
+            {
+                return _driver.FindElement(By.XPath(
+                        "/html/body/div[4]/div/div/div/div/div[2]/section/div/div[2]/div[3]/section/div/div/div/div[2]/div/div[2]/div[3]/section/div/div/div/div/div/div[3]/div/div/div/section/div[2]/div[2]/div/div/ul"));
+
+                ;
+            }
+        }
+
+
+      
+
+
         public Func<IWebDriver, IWebElement> ElementIsClickable(By locator)
         {
 
@@ -104,7 +145,112 @@ namespace LloydsDCAutomation
 
         }
 
+        public IWebElement DescriptionOfAttachment
+        {
+            get
+            {
+                return _wait.Until(x =>
+                    x.FindElement(By.XPath(
+                        "/html/body/div[1]/div[2]/section/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div/div/div")));
+            }
+        }
 
+        public IWebElement attachmentsChooseSupplier
+        {
+            get
+            {
+                return _wait.Until(x => x.FindElement(By.XPath("/html/body/div[1]/div[3]/div/div/ul")));
+            }
+        }
+
+
+        public string Suppliers()
+        {
+
+            var polygonSuppliers = new[]
+            {
+                $"Alpine Construction",
+                "Armistead Buildings Services (ABS)",
+                "Betacove Ltd",
+                "Buildings Validation Solutions Ltd",
+                "Camilleri Construction",
+                "Capital Services",
+                "Clark Contracts Ltd",
+                "Crawfords General and Subs - LA",
+                "Dale Building Maintenance",
+                "EBL GROUP",
+                "Alpine Construction",
+                "Armistead Buildings Services (ABS)",
+                "Betacove Ltd",
+                "Buildings Validation Solutions Ltd",
+                "Camilleri Construction",
+                "Capital Services",
+                "Clark Contracts Ltd",
+                "Crawfords General and Subs - LA",
+                "Dale Building Maintenance",
+                "EBL GROUP",
+                "Edinmore Contracts Ltd",
+                "Heightvale",
+                "Hemmings & Marshalsea",
+                "HRNL",
+                "Marlowe Industries Ltd",
+                "McCane Construction",
+                "MIDLANDS RESTORATION LTD",
+                "Neways",
+                "North East Building Services Ltd",
+                "Phoenix Building Services",
+                "Reeve Property Restoration Ltd",
+                "Regent Develpoment",
+                "RFM Group",
+                "Roywood Contractors Ltd",
+                "T Denman & Sons Ltd",
+                "TBRN",
+                "The Cotswold Group"
+            };
+
+
+
+
+
+            ReadOnlyCollection<IWebElement> participants = this.ParticipantsList.FindElements(By.ClassName("sapMSLITitle"));
+
+            List<string> membersList = new List<string> { };
+
+            var polygon = new string("");
+
+            foreach (var company in participants)
+            {
+                
+                string member = company.Text;
+
+                membersList.Add(member);
+                
+
+
+            }
+
+            
+            foreach (var supplier in polygonSuppliers)
+            {
+
+                foreach (var member in membersList)
+                {
+                    if (supplier.Contains(member))
+                    {
+                        polygon = member;
+
+                    }
+                    else return null;
+
+                }
+
+            }
+
+           return  polygon;
+
+
+
+        }
 
     }
 
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Dynamic;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
 using System.Threading;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
 using OpenQA.Selenium.Interactions;
 using OpenQA.Selenium.Support.UI;
 
@@ -18,16 +26,18 @@ namespace LloydsDCAutomation
 
         public Program(ElementList elementList)
         {
-           
+
         }
 
 
         static void Main(string[] args)
         {
-            var options = new ChromeOptions();
-            var driver = new ChromeDriver(options);
+            var options = new EdgeOptions();
 
-            var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(40));
+            var driver = new EdgeDriver(options);
+
+
+            var wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver, TimeSpan.FromSeconds(40));
             var elements = new ElementList(driver, wait);
 
 
@@ -38,25 +48,25 @@ namespace LloydsDCAutomation
             elements.Launch();
 
 
-            var insuranceRef = args[0].Substring(57, 9);
+            var insuranceRef = args[0].Substring(args[0].LastIndexOf(":", StringComparison.Ordinal) + 2);
 
-            elements.Username.SendKeys("F000254");
+            elements.Username.SendKeys("F000359");
 
-            elements.Password.SendKeys("Zovan18!");
+            elements.Password.SendKeys("Coconut2!");
 
             elements.Login.Click();
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
 
-           
+
             elements.Supplier.Click();
 
 
 
             driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(120);
 
-            
-            
+
+
 
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
@@ -66,7 +76,7 @@ namespace LloydsDCAutomation
                     By.XPath("//*[@id=\"application-ZSupplierSemObj-display-component---master--searchField-I\"]")));
             searchClaim.SendKeys(insuranceRef);
 
-            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(120);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(110);
 
 
 
@@ -74,26 +84,64 @@ namespace LloydsDCAutomation
 
 
 
-            Thread.Sleep(TimeSpan.FromSeconds(100));
+            Thread.Sleep(TimeSpan.FromSeconds(80));
+
+            elements.Participants.Click();
+            //var participantLisa = driver.FindElement(By.XPath(
+            //    "/html/body/div[4]/div/div/div/div/div[2]/section/div/div[2]/div[3]/section/div/div/div/div[2]/div/div[2]/div[3]/section/div/div/div/div/div/div[3]/div/div/div/section/div[2]/div[2]/div/div/ul"));
+
+
+
+            var participants =
+
+                driver.FindElement(By.XPath(
+                    "/html/body/div[4]/div/div/div/div/div[2]/section/div/div[2]/div[3]/section/div/div/div/div[2]/div/div[2]/div[3]/section/div/div/div/div/div/div[3]/div/div/div/section/div[2]/div[2]/div/div/ul"));
+
+
+            Console.WriteLine(elements.Suppliers());
 
 
             elements.Attachments.Click();
-
             elements.AddAttachments.Click();
+
+            //This is where I'm trying to switch to the popup
+
+            driver.SwitchTo().Frame("id-1601915183555-71");
+
+           
+            elements.DescriptionOfAttachment.SendKeys("Polygon DC");
+
+           
+
+
+            elements.attachmentsChooseSupplier.SendKeys(elements.Suppliers());
+
+
+
+            Console.WriteLine($"{participants.Text} This is from the p");
+
+
 
             driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(60);
 
 
-            //builder.SendKeys(Keys.Enter);
+
+          
 
 
 
             Console.WriteLine("Hello World!");
 
 
-            //        }
-            //}
-            //}
+
         }
+
+
+
+
+
+
+
+
     }
 }
